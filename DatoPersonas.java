7f062db6ccfda99c65de587bda390585c82f7c3d
Valentin <valentincrippa@gmail.com>
2018-05-28 19:17:43 -0300

    Ejercicio 8

---
+++ DatoPersonas.java
@@ -0,0 +1,19 @@
+package test;
+
+import test.Empleado;
+import java.util.Scanner;
+
+public class DatoPersonas{
+
+	public static void main(String ar[]) {
+		
+		Empleado Empleado1;
+		Empleado1 = new Empleado();
+		Empleado1.CargarSueldo();
+		Empleado1.ImprimirSueldo();
+	}
+
+}
+
+	
+	
