b6ce0bc43d952f507af45a3a374d05228583aaec
Valentin <valentincrippa@gmail.com>
2018-05-28 19:30:08 -0300

    Ejercicio 4

---
+++ TablaDel5.java
@@ -0,0 +1,18 @@
+
+public class TablaDel5 {
+
+	public static void main(String ar[]) {
+		
+		int num = 5; 
+		
+		for(int i = 0; i < 10; i++) {
+			
+			System.out.print(num + "_");
+			
+			num = num + 5;
+			
+		}
+		
+	}
+	
+}
